d67ea68adb2cfc344b79bd96c3aa4acab16c2bf4
Andrey <757grok@mail.ru>
2019-05-08 12:23:09 +0700

    Удаление сущности ReceiptPaymentFines
    Удалил сущность, так как она подходит на будущий функционал, но не к требуемому текущему функционалу.

--- src/main/java/com/test/carfines/repository/ReceiptPaymentFinesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.test.carfines.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceiptPaymentFinesRepository extends JpaRepository<ReceiptPaymentFinesRepository,Long> {
-}
