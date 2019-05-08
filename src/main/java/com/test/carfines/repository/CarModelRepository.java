308e0ec2c18d8de29a1f8fee88088635eb22c459
Andrey <757grok@mail.ru>
2019-05-08 13:46:13 +0700

    Добавил функцию поиска в CarModelRepository
    Поиск по имени модели автомобиля

--- src/main/java/com/test/carfines/repository/CarModelRepository.java
+++ src/main/java/com/test/carfines/repository/CarModelRepository.java
@@ -1,7 +1,12 @@
 package com.test.carfines.repository;
 
 import com.test.carfines.model.CarModel;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CarModelRepository extends JpaRepository<CarModel, Long> {
+    @Query("select m from CarModel m where m.carModelName = :name")
+    CarModel findByName(@Param("name") String carModelName);
 }
