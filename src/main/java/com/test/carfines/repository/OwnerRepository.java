212f8eb6127b9bafc14f072954abd32abb9ad12f
Andrey <757grok@mail.ru>
2019-05-08 16:41:36 +0700

    Добавил в репы функции поиска
    Функции поиска по имени

--- src/main/java/com/test/carfines/repository/OwnerRepository.java
+++ src/main/java/com/test/carfines/repository/OwnerRepository.java
@@ -1,7 +1,12 @@
 package com.test.carfines.repository;
 
 import com.test.carfines.model.Owner;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OwnerRepository extends JpaRepository<Owner,Long> {
+    @Query("select b from Owner b where b.nameOwner = :name")
+    Owner findByName(@Param("name") String ownerName);
 }
