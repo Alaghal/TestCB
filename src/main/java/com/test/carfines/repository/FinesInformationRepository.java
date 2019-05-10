edde863aff976847cd03f9f0851e0063f34d07a9
Andrey <757grok@mail.ru>
2019-05-10 14:38:37 +0700

    Добавление нового функционала в репозитории
    Добавил функцию поиска по модели и владельцу для репозитория гос номера

--- src/main/java/com/test/carfines/repository/FinesInformationRepository.java
+++ src/main/java/com/test/carfines/repository/FinesInformationRepository.java
@@ -1,7 +1,10 @@
 package com.test.carfines.repository;
 
+
 import com.test.carfines.model.FinesInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface FinesInformationRepository extends JpaRepository<FinesInformation, Long> {
+
 }
