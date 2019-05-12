0613b78cbbf7cf2e3d06c7e528b63307d6a5a58f
Andrey <757grok@mail.ru>
2019-05-13 03:04:30 +0700

    Правки контроллера
    Добавлена валидация, убраны лишнии мапинги, оптимизация импортов

--- src/main/java/com/test/carfines/controler/MainController.java
+++ src/main/java/com/test/carfines/controler/MainController.java
@@ -2,12 +2,17 @@ package com.test.carfines.controler;
 
 import com.test.carfines.domain.FinesInformation;
 import com.test.carfines.domain.FrequentFinesDTO;
+import com.test.carfines.domain.LicensePlateNumber;
+import com.test.carfines.domain.Owner;
 import com.test.carfines.service.impl.FinesInformationServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -18,31 +23,61 @@ public class MainController {
         this.service = finesInformation;
     }
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
 
-
-    @GetMapping("/filter")
-    public String filter(Model model){
+    @GetMapping({"/", "/main"})
+    public String filter(Model model) {
+        Iterable<FinesInformation> finesInformations = service.getAll();
+        model.addAttribute( "finesInformations", finesInformations );
+        model = addAttributeForFoundingForm( model );
         return "main";
     }
 
     @GetMapping("/report")
-    public String getReport(Model model ){
+    public String getReport(Model model) {
         Iterable<FrequentFinesDTO> finesDTOS = service.getFrequentFines();
-        model.addAttribute("frequentFines", finesDTOS  );
+        model.addAttribute( "frequentFines", finesDTOS );
         return "frequentfines";
     }
 
-    @PostMapping("/filter2")
-     public String filter (@RequestParam String filterNameOwner,
-                           @RequestParam String filterLPN,
-                           Model model){
-        Iterable<FinesInformation> finesInformations =service.getFinesInformation( filterNameOwner,filterLPN );
+    private Model addAttributeForFoundingForm(Model model) {
+        model.addAttribute( "owner", new Owner() );
+        model.addAttribute( "licensePlateNumber", new LicensePlateNumber() );
+        return model;
+    }
+
+    @PostMapping("/filter")
+    public String filter(@Valid Owner owner, BindingResult bindingResultOwner,
+                         @Valid LicensePlateNumber licensePlateNumber,
+                         BindingResult bindingResultLPN,
+                         Model model) {
+        model = addAttributeForFoundingForm( model );
+
+        if (bindingResultOwner.hasErrors() && bindingResultLPN.hasErrors()) {
+            Map<String, String> mapOwnersErrors = ControllerUtils.getErrors( bindingResultOwner );
+            Map<String, String> mapLPNErrors = ControllerUtils.getErrors( bindingResultLPN );
+
+            model.addAllAttributes( mapOwnersErrors );
+            model.addAllAttributes( mapLPNErrors );
+
+            return "main";
+        }
+
+        if (bindingResultOwner.hasErrors()) {
+            Map<String, String> mapOwnersErrors = ControllerUtils.getErrors( bindingResultOwner );
+            model.addAllAttributes( mapOwnersErrors );
+            return "main";
+        }
+
+        if (bindingResultLPN.hasErrors()) {
+            Map<String, String> mapLPNErrors = ControllerUtils.getErrors( bindingResultLPN );
+            model.addAllAttributes( mapLPNErrors );
+            return "main";
+        }
+
+
+        Iterable<FinesInformation> finesInformations = service.getFinesInformation( owner.getNameOwner(), licensePlateNumber.getLicensePlateNumbersName() );
         model.addAttribute( "finesInformations", finesInformations );
+
         return "main";
     }
 }
